47f3ae5b8c99a6ed1cfd3487a9a6a1fcc2efebd6
CamiloPabon <camilo.rengifo.pabon@gmail.com>
2025-04-06 02:25:45 -0500

    funcional de listview y details con api chucknorris

--- lib/chuknorrys/chuk_list_view.dart
+++ lib/chuknorrys/chuk_list_view.dart
@@ -23,58 +23,55 @@ class _ChucknorrisListViewState extends State<ChucknorrisListView> {
 
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      title: 'Bromas de Chuck Norris',
-      body: FutureBuilder<List<Chuknoris>>(
-        future: _futureChuckList,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final chuckNorrisList = snapshot.data!;
-            return ListView.builder(
-              itemCount: chuckNorrisList.length,
-              itemBuilder: (context, index) {
-                final chuckNorris = chuckNorrisList[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      context.push('/chucknorris/${chuckNorris.id}');
-                    },
-                    child: Card(
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: Image.network(chuckNorris.icon_url, width: 80, height: 80, fit: BoxFit.cover),
+    return FutureBuilder<List<Chuknoris>>(
+      future: _futureChuckList,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final chuckNorrisList = snapshot.data!;
+          return ListView.builder(
+            itemCount: chuckNorrisList.length,
+            itemBuilder: (context, index) {
+              final chuckNorris = chuckNorrisList[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    GoRouter.of(context).push('/chucknorris/${chuckNorris.id}');
+                  },
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Image.network(chuckNorris.icon_url, width: 80, height: 80, fit: BoxFit.cover),
+                          ),
+                          const SizedBox(width: 16.0),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(chuckNorris.value.toUpperCase(), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                              ],
                             ),
-                            const SizedBox(width: 16.0),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(chuckNorris.value.toUpperCase(), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                                ],
-                              ),
-                            ),
-                            const Icon(Icons.chevron_right),
-                          ],
-                        ),
+                          ),
+                          const Icon(Icons.chevron_right),
+                        ],
                       ),
                     ),
                   ),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
-      ),
+                ),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,50 +12,74 @@ import 'chuknorrys/chucknorris_detail_screen.dart';
 import 'chuknorrys/chuk_list_view.dart';
 import 'screens/home_screen.dart';
 
-// Función principal que se ejecuta al inicio de la aplicación.
 Future<void> main() async {
-  // Asegura que los bindings de Flutter estén inicializados.
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Carga las variables de entorno desde el archivo .env.
-  // Asegúrate de que el archivo .env está en la raíz de tu proyecto.
   await dotenv.load(fileName: ".env");
-
-  // Ejecuta la aplicación MyApp.
   runApp(const MyApp());
 }
 
-// Widget principal de la aplicación.
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const MainScreen(),
+    ),
+    GoRoute(
+      path: '/detail/:id',
+      builder: (context, state) {
+        int id = int.parse(state.pathParameters['id']!);
+        return DetailScreen(id: id);
+      },
+    ),
+    GoRoute(
+      path: '/ciclo_vida',
+      builder: (context, state) => CicloVidaScreen(),
+    ),
+    GoRoute(
+      path: '/estudiantes',
+      builder: (context, state) => const ListaEstudiantesScreen(),
+    ),
+    GoRoute(
+      path: '/contador',
+      builder: (context, state) => const ContadorScreen(),
+    ),
+    GoRoute(
+      path: '/tarea_pesada',
+      builder: (context, state) => const TareaPesadaScreen(),
+    ),
+    GoRoute(
+      path: '/chucknorris',
+      builder: (context, state) => ChucknorrisListView(),
+    ),
+    GoRoute(
+      path: '/chucknorris/:id',
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        return ChuckNorrisDetailScreen(id: id);
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
-  // Constructor de MyApp.
   const MyApp({super.key});
 
-  // Método que construye el widget de la aplicación.
   @override
   Widget build(BuildContext context) {
-    // Retorna un widget MaterialApp que define la estructura básica de la aplicación.
-    return MaterialApp(
-      // Establece la pantalla principal de la aplicación a MainScreen.
-      home: const MainScreen(),
-      // Desactiva el banner de modo de depuración.
+    return MaterialApp.router(
+      routerConfig: router,
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// Widget que representa la pantalla principal de la aplicación.
 class MainScreen extends StatelessWidget {
-  // Constructor de MainScreen.
   const MainScreen({super.key});
 
-  // Método que construye el widget de la pantalla principal.
   @override
   Widget build(BuildContext context) {
-    // Retorna un widget BaseView que contiene el TabBar y el TabBarView.
     return BaseView(
-      // Título de la AppBar.
       title: 'Gestión de Funcionalidades',
-      // Lista de pestañas para el TabBar.
       tabs: const [
         Tab(icon: Icon(Icons.home), text: 'Inicio'),
         Tab(icon: Icon(Icons.star), text: 'Favoritos'),
@@ -65,7 +89,6 @@ class MainScreen extends StatelessWidget {
         Tab(icon: Icon(Icons.work), text: 'Tarea Pesada'),
         Tab(icon: Icon(Icons.emoji_emotions), text: 'Chuck Norris'),
       ],
-      // Lista de widgets para el TabBarView.
       children: const [
         HomeScreen(),
         FavoriteScreen(),
@@ -79,17 +102,12 @@ class MainScreen extends StatelessWidget {
   }
 }
 
-// Widget que representa la pantalla de favoritos.
 class FavoriteScreen extends StatelessWidget {
-  // Constructor de FavoriteScreen.
   const FavoriteScreen({super.key});
 
-  // Método que construye el widget de la pantalla de favoritos.
   @override
   Widget build(BuildContext context) {
-    // Obtiene la lista de favoritos desde el FavoritesManager.
     final favorites = FavoritesManager.favorites;
-    // Retorna un widget ListView.builder que muestra la lista de favoritos.
     return ListView.builder(
       itemCount: favorites.length,
       itemBuilder: (context, index) {
@@ -103,68 +121,13 @@ class FavoriteScreen extends StatelessWidget {
   }
 }
 
-// Widget que representa la pantalla de ajustes.
 class SettingsScreen extends StatelessWidget {
-  // Constructor de SettingsScreen.
   const SettingsScreen({super.key});
 
-  // Método que construye el widget de la pantalla de ajustes.
   @override
   Widget build(BuildContext context) {
-    // Retorna un widget Center con un texto de ajustes.
     return const Center(
       child: Text('Ajustes', style: TextStyle(fontSize: 24)),
     );
   }
 }
-
-// Definición del enrutador de navegación utilizando GoRouter.
-final GoRouter router = GoRouter(
-  // Lista de rutas de la aplicación.
-  routes: [
-    // Ruta para la pantalla de detalles.
-    GoRoute(
-      // Ruta de acceso (path) con un parámetro dinámico ':id'.
-      path: '/detail/:id',
-      // Función que construye la pantalla de detalles.
-      builder: (context, state) {
-        // Convierte el parámetro 'id' a un entero.
-        int id = int.parse(state.pathParameters['id']!);
-        // Retorna el widget de la pantalla de detalles (DetailScreen) con el ID.
-        return DetailScreen(id: id);
-      },
-    ),
-    // Ruta para la pantalla del ciclo de vida.
-    GoRoute(
-      // Ruta de acceso (path) para la pantalla del ciclo de vida.
-      path: '/ciclo_vida',
-      // Función que construye la pantalla del ciclo de vida.
-      builder: (context, state) {
-        // Retorna el widget de la pantalla del ciclo de vida (CicloVidaScreen).
-        return CicloVidaScreen();
-      },
-    ),
-    // Ruta para la lista de bromas de Chuck Norris.
-    GoRoute(
-      // Ruta de acceso (path) para la lista de bromas de Chuck Norris.
-      path: '/chucknorris',
-      // Función que construye la lista de bromas de Chuck Norris.
-      builder: (context, state) {
-        // Retorna el widget de la lista de bromas de Chuck Norris (ChucknorrisListView).
-        return ChucknorrisListView();
-      },
-    ),
-    // Ruta para los detalles de una broma de Chuck Norris.
-    GoRoute(
-      // Ruta de acceso (path) con un parámetro dinámico ':id'.
-      path: '/chucknorris/:id',
-      // Función que construye los detalles de una broma de Chuck Norris.
-      builder: (context, state) {
-        // Obtiene el ID de los parámetros de la ruta.
-        final id = state.pathParameters['id']!;
-        // Retorna el widget de los detalles de la broma de Chuck Norris (ChuckNorrisDetailScreen) con el ID.
-        return ChuckNorrisDetailScreen(id: id);
-      },
-    ),
-  ],
-);
